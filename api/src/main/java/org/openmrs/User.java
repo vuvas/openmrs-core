7ad5e6b5c8ebbd2a268721a8d7928d187f801f0f
Yonas Tesfaye <vuvas@users.noreply.github.com>
2026-09-12 00:04:58 +0300

    Update api/src/main/java/org/openmrs/User.java
    Co-authored-by: Ian <52504170+ibacher@users.noreply.github.com>

--- api/src/main/java/org/openmrs/User.java
+++ api/src/main/java/org/openmrs/User.java
@@ -108,7 +108,7 @@ public class User extends BaseOpenmrsObject implements java.io.Serializable, Att
 	@ManyToMany(fetch = FetchType.LAZY)
 	@JoinTable(name = "user_location", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "location_id"))
 	@Independent
-	private Set<Location> locations = new HashSet<>();
+	private Set<Location> assignedLocations = new LinkedHashSet<>();
 
 	@ElementCollection
 	@CollectionTable(name = "user_property", joinColumns = @JoinColumn(name = "user_id", nullable = false))
